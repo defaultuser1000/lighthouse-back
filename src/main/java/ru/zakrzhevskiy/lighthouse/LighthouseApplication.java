263d44ea29a6851c95b025456ef1090a4350394c
Andrey Zakrzhevskiy <and.zak.one@gmail.com>
2020-08-13 02:06:15 +0300

    Removed OS identification on startup

--- src/main/java/ru/zakrzhevskiy/lighthouse/LighthouseApplication.java
+++ src/main/java/ru/zakrzhevskiy/lighthouse/LighthouseApplication.java
@@ -13,9 +13,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class LighthouseApplication {
 
 	public static void main(String[] args) {
-		System.out.println(System.getProperty("os.arch"));
-		System.out.println(System.getProperty("os.name"));
-		System.out.println(System.getProperty("os.version"));
 		SpringApplication.run(LighthouseApplication.class, args);
 	}
 
